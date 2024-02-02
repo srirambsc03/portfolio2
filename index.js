9f721428d9f41530634c72da6bd225ab6e64131d
srirambsc03 <145130157+srirambsc03@users.noreply.github.com>
2024-02-02 07:19:07 -1100

    commit

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction(){
+    let menuBtn = document.getElementById("NavMenu");
+
+    if(menuBtn.className === "nav-menu"){
+      menuBtn.className += " responsive";
+    } else {
+      menuBtn.className = "nav-menu";
+    }
+  }
+
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+  window.onscroll = function() {headerShadow()};
+
+  function headerShadow() {
+    const navHeader =document.getElementById("header");
+
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+
+      navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+      navHeader.style.height = "70px";
+      navHeader.style.lineHeight = "70px";
+
+    } else {
+
+      navHeader.style.boxShadow = "none";
+      navHeader.style.height = "90px";
+      navHeader.style.lineHeight = "90px";
+
+    }
+  }
 /* ----- TYPING EFFECT ----- */
  let typingEffect = new Typed(".typedText",{
     strings : ["Web Developer","Web Designer","Full Stack Developer"],
